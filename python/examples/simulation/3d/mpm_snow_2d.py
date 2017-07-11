38e05dea51a0fa10882c69694ddaafca196694c6
Yuanming Hu <iterator.advance@gmail.com>
2017-07-11 17:02:55 -0400

    BC for IMEX

--- python/examples/simulation/3d/mpm_snow_2d.py
+++ python/examples/simulation/3d/mpm_snow_2d.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
 
     res = (256 / downsample, 256 / downsample)
 
-    mpm = tc.dynamics.MPM(res=res, delta_x=1.0 / res[0], gravity=(0, 0, 0), base_delta_t=0.001, num_threads=1,
+    mpm = tc.dynamics.MPM(res=res, delta_x=1.0 / res[0], gravity=(0, 0, 0), base_delta_t=0.002, num_threads=1,
                           implicit_ratio=1.0, implicit_solve_tolerance=1e-6, implicit_solve_iteration_limit=30)
 
     levelset = mpm.create_levelset()
@@ -17,13 +17,13 @@ if __name__ == '__main__':
     # levelset.add_plane(tc.Vector(0, 1), -0.2)
     # mpm.set_levelset(levelset, False)
 
-    #tex_ball = tc.Texture('sphere', center=(0.35, 0.5, 0.5), radius=0.08) * 10
-    #mpm.add_particles(density_tex=tex_ball.id, initial_velocity=(0.5, 0, 0), compression=0.9)
+    tex_ball = tc.Texture('sphere', center=(0.5, 0.7, 0.5), radius=0.10) * 4
+    mpm.add_particles(density_tex=tex_ball.id, initial_velocity=(0, -0.3, 0), compression=1.0)
 
-    tex_ball = tc.Texture('sphere', center=(0.8, 0.5, 0.5), radius=0.08) * 10
-    mpm.add_particles(density_tex=tex_ball.id, initial_velocity=(-0.3, 0.1, 0), compression=1.0)
+    tex_ball = tc.Texture('sphere', center=(0.5, 0.3, 0.5), radius=0.08) * 4
+    mpm.add_particles(density_tex=tex_ball.id, initial_velocity=(0, 0.3, 0), compression=1.0)
 
-    frame_dt = 0.03
+    frame_dt = 0.06
 
     t = 0
     for i in range(num_steps):
--- src/simulation/mpm/mpm_implicit.cpp
+++ src/simulation/mpm/mpm_implicit.cpp
@@ -22,7 +22,7 @@ void MPM<2>::implicit_velocity_update(real delta_t) {
 
     // The explicit part is not involved in this solve and is used only as boundary conditions.
 
-    auto apply = [&](const Array &x_, Array &y) {
+    auto apply = [&](const Array &x_, Array &y, bool BC = false) {
         // x: input
         // y: output
 
@@ -31,10 +31,21 @@ void MPM<2>::implicit_velocity_update(real delta_t) {
         // Calculate Ap : Grid -> Particle
 
         for (auto &ind: imex_mask.get_region()) {
-            if (imex_mask[ind] == 0) {
-                x[ind] = Vector(0.0f);
+            if (!BC) {
+                // normal iteration
+                if (imex_mask[ind] == 0) {
+                    x[ind] = Vector(0.0f);
+                }
+            } else {
+                // initial BC calculation
+                if (imex_mask[ind] == 0) {
+                    x[ind] = grid_velocity[ind];
+                } else {
+                    x[ind] = Vector(0.0f);
+                }
             }
         }
+
         for (auto &p : scheduler.get_active_particles()) {
             const Vector pos = p->pos * inv_delta_x;
             Kernel kernel(pos, inv_delta_x);
@@ -50,7 +61,6 @@ void MPM<2>::implicit_velocity_update(real delta_t) {
                 dF += Matrix::outer_product(dw, x[i]);
             }
             dF = dF * p->dg_e;
-
             p->Ap = p->get_first_piola_kirchoff(dF);
         }
 
@@ -73,17 +83,22 @@ void MPM<2>::implicit_velocity_update(real delta_t) {
             if (imex_mask[ind] > 0) {
                 y[ind] = (implicit_ratio * pow<2>(delta_t)) * y[ind] + x[ind] * grid_mass[ind];
             } else {
-                y[ind] = Vector(0.0f);
+                y[ind] = Vector(0.0f);//grid_velocity[ind] * grid_mass[ind];
             }
         }
     };
 
     Array x = grid_velocity.same_shape(), rhs = grid_velocity.same_shape();
+    Array rhs_delta = grid_velocity.same_shape();
+    apply(grid_velocity, rhs_delta, true);
+
     for (auto &ind: grid_velocity.get_region()) {
         if (imex_mask[ind] > 0) {
+            // Implicit region
             x[ind] = grid_velocity[ind];
-            rhs[ind] = grid_velocity[ind] * grid_mass[ind];
+            rhs[ind] = grid_velocity[ind] * grid_mass[ind] - rhs_delta[ind];
         } else {
+            // Explicit region
             x[ind] = Vector(0.0f);
             rhs[ind] = Vector(0.0f);
         }
@@ -114,7 +129,7 @@ void MPM<2>::implicit_velocity_update(real delta_t) {
         x = x.add(alpha, p);
         r = r.add(-alpha, Ap);
         real d_error = sqrt(dot(r, r));
-        // printf("CR error: %.10f  log=%.2f\n", d_error, log(d_error) / log(10.0f));
+        printf("CR error: %.10f  log=%.2f\n", d_error, log(d_error) / log(10.0f));
         if (d_error < implicit_solve_tolerance) {
             printf("CR converged at iteration %d\n", k + 1);
             early_break = true;
